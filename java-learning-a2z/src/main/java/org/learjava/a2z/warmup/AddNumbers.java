d4e902da9214d55c8254ae10e044f9d8d5a75034
karthik_java_swe <karthik.java.swe@gmail.com>
2023-10-15 14:24:24 +0530

    Add two numbers in java

---
+++ java-learning-a2z/src/main/java/org/learjava/a2z/warmup/AddNumbers.java
@@ -0,0 +1,10 @@
+package org.learjava.a2z.warmup;
+
+public class AddNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int result = num1 + num2;
+        System.out.println("result after adding num1 and num2 is :: " + result);
+    }
+}
